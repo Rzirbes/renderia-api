0e02ce692ffb93812db2f34d2780e88f632c4d06
Romulo Zirbes <62040520+Rzirbes@users.noreply.github.com>
2026-03-05 21:21:26 -0300

    redis

--- src/modules/renders/renders.queue.ts
+++ src/modules/renders/renders.queue.ts
@@ -1,6 +1,5 @@
-// src/queue/queues.ts
 import { Queue } from 'bullmq';
-import { bullConnection } from 'src/queue/connection';
+import { bullConnection } from '../../queue/connection';
 
 export const rendersQueue = new Queue('renders', {
   connection: bullConnection,
--- src/queue/connection.ts
+++ src/queue/connection.ts
@@ -1,9 +1,6 @@
 // src/queue/connection.ts
 import type { ConnectionOptions } from 'bullmq';
 
-export const bullConnection: ConnectionOptions = process.env.REDIS_URL
-  ? { url: process.env.REDIS_URL }
-  : {
-      host: process.env.REDIS_URL ?? 'localhost',
-      port: Number(process.env.REDIS_PORT ?? 6379),
-    };
+export const bullConnection: ConnectionOptions = {
+  url: process.env.REDIS_URL,
+};
